81045bafd8e896d81704106445cf414524fbf99f
dodolidad <dodolidad@gmail.com>
2022-11-05 03:53:39 +0900

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const mybatisMapper = require('mybatis-mapper');
 const format = {language: 'sql', indent: '  '};
 require("dotenv").config();
@@ -10,6 +10,10 @@ const util = require('util');
 const client = new textToSpeech.TextToSpeechClient();
 const jwt = require('jsonwebtoken');
 var macaddress = require('macaddress');
+const path = require('path');
+
+let decoded = null;
+let decoded1 = null;
 
 const pool = mysql.createPool({
   connectionLimit : 10,
@@ -21,8 +25,125 @@ const pool = mysql.createPool({
 
 mybatisMapper.createMapper([ './sql.xml' ]);
 
+router.post('/setText', async function(req, res, next) {
+  const param = req.body;
 
-router.post('/join', function async(req, res, next) {
+  //유효성 체크
+  let chkValidResult = await chkValid('/setText', param);
+  if(!chkValidResult.success) {
+    res.send(chkValidResult);
+    return;
+  }
+
+  param.userId = decoded.userId;
+  param.textId = Date.now();
+
+  let textKrs = param.textKr.split('\n');
+
+  for(let i = 1; i <= textKrs.length; i++) {
+    let textKr = textKrs[i - 1].substr(5);
+    let textVoice = textKrs[i - 1].substr(0, 5);
+    let textName = '';
+    let outputFile = './sounds/' + param.textId + i + 'kr.mp3';
+
+    if(textVoice === '남1 : ') textName = 'ko-KR-Wavenet-C'
+    else if(textVoice === '남2 : ') textName = 'ko-KR-Wavenet-D'
+    else if(textVoice === '여1 : ') textName = 'ko-KR-Wavenet-B'
+    else if(textVoice === '여2 : ') textName = 'ko-KR-Wavenet-A'
+
+    const request = {
+      input: {text: textKr},
+      voice: {languageCode: 'ko-KR', name: textName},
+      audioConfig: {audioEncoding: 'MP3'},
+    };
+    try{
+      const [response] = await client.synthesizeSpeech(request);
+      const writeFile = util.promisify(fs.writeFile);
+      await writeFile(outputFile, response.audioContent, 'binary');
+    }catch(err) {
+      res.send({success: false, msg: '음성파일 저장 실패.'});
+      return;
+    }
+  }
+
+  let textEns = param.textEn.split('\n');
+
+  for(let i = 1; i <= textEns.length; i++) {
+    let textEn = textEns[i - 1].substr(5);
+    let textVoice = textEns[i - 1].substr(0, 5);
+    let textName = '';
+    let outputFile = './sounds/' + param.textId + i + 'en.mp3';
+
+    if(textVoice === 'M1 : ') textName = 'en-US-Wavenet-A'
+    else if(textVoice === 'M2 : ') textName = 'en-US-Wavenet-B'
+    else if(textVoice === 'F1 : ') textName = 'en-US-Wavenet-C'
+    else if(textVoice === 'F2 : ') textName = 'en-US-Wavenet-F'
+
+    const request = {
+      input: {text: textEn},
+      voice: {languageCode: 'en-US', name: textName},
+      audioConfig: {audioEncoding: 'MP3'},
+    };
+    try{
+      const [response] = await client.synthesizeSpeech(request);
+      const writeFile = util.promisify(fs.writeFile);
+      await writeFile(outputFile, response.audioContent, 'binary');
+    }catch(err) {
+      res.send({success: false, msg: '음성파일 저장 실패.'});
+      return;
+    }
+  }
+
+  const connection = await pool.getConnection(async conn => conn);
+
+  try {
+    await connection.beginTransaction();
+    
+    for(let i = 1; i <= textKrs.length; i++) {
+      let newParam = {textId: param.textId, idx: i, textKr: textKrs[i - 1], textEn: textEns[i - 1], userId: param.userId};
+
+      await connection.query(mybatisMapper.getStatement('youngjak', 'inserText', newParam, format));
+
+      if(i === 1) await connection.query(mybatisMapper.getStatement('youngjak', 'inserUserText', newParam, format));
+    }
+
+    await connection.commit();
+  } catch (err) {
+      await connection.rollback();
+
+      res.send({success: false, msg: '데이터 저장 실패.'});
+      return;
+  } finally {
+      connection.release();
+  }
+
+  res.send({success: true, msg: '데이터 저장 성공.'});
+});
+
+router.get('/getTextOneRandomForGuest', async function(req, res, next) {
+  const connection = await pool.getConnection(async conn => conn);
+
+  try {
+    const [results, fields] = await connection.query(mybatisMapper.getStatement('youngjak', 'getTextOneRandomForGuest', {}, format));
+    res.send({success: true, msg: '', results});
+  } catch (err) {
+      res.send({success: false, msg: '데이터 조회 실패.'});
+      return;
+  } finally {
+      connection.release();
+  }
+});
+
+router.get('/getSound', async function(req, res, next) {
+  try{
+    let filePath = path.join(__dirname, '../sounds/' + req.query.textId + req.query.idx + req.query.lang + '.mp3');
+    res.sendFile(filePath);
+  } catch(err) {
+
+  }
+});
+
+router.post('/join', async function(req, res, next) {
   const param = req.body;
   const idRegExp = /^[a-zA-z0-9]{4,12}$/;
   const pwdRegExp = /^[a-zA-z0-9]{4,12}$/;
@@ -60,50 +181,41 @@ router.post('/join', function async(req, res, next) {
     return;
   }
 
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      console.log(err);
-      res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
+  const connection = await pool.getConnection(async conn => conn);
+
+  try {
+    await connection.beginTransaction();
+
+    let [results] = await connection.query(mybatisMapper.getStatement('youngjak', 'getUserIdCnt', param, format));
+
+    if(results[0].USER_ID_CNT === 1) {
+      connection.release();
+      res.send({success: false, msg: '이미 존재하는 아이디입니다.'});
       return;
     }
 
-    connection.query(mybatisMapper.getStatement('youngjak', 'getUserIdCnt', param, format), function (error, results, fields) {
-      if (error) {
-        console.log(error);
-        res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-        connection.release();
-        return;
-      }
+    [results] = await connection.query(mybatisMapper.getStatement('youngjak', 'insertUser', param, format));
 
-      if(results[0].USER_ID_CNT === 1) {
-        res.send({success: false, msg: '이미 존재하는 아이디입니다.'});
-        connection.release();
-        return;
-      }
+    if(results.affectedRows !== 1) {
+      connection.release();
+      res.send({success: false, msg: '데이터 저장 실패.'});
+      return;
+    }
 
-      connection.query(mybatisMapper.getStatement('youngjak', 'insertUser', param, format), function (error, results, fields) {
-        if (error) {
-          console.log(error);
-          res.send({success: 1, msg: '데이터 처리 실패. 다시 시도하세요.'});
-          connection.release();
-          return;
-        }
+    await connection.commit();
+  } catch (err) {
+      await connection.rollback();
 
-        if(results.affectedRows != 1) {
-          res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-          connection.release();
-          return;
-        }
+      res.send({success: false, msg: '데이터 저장 실패.'});
+      return;
+  } finally {
+      connection.release();
+  }
 
-        connection.release();
-    
-        res.send({success: true, msg: '회원가입을 축하합니다.'});
-      });
-    });
-  });
+  res.send({success: true, msg: '회원가입을 축하합니다.'});
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', async function(req, res, next) {
   const param = req.body;
 
   if(param.userId === undefined || param.userId === null) {
@@ -114,254 +226,87 @@ router.post('/login', function(req, res, next) {
     return;
   }
 
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-      return;
-    }
-
-    connection.query(mybatisMapper.getStatement('youngjak', 'getUserIdCnt', param, format), function (error, results, fields) {
-      if (error) {
-        res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-        connection.release();
-        return;
-      }
-
-      if(results[0].USER_ID_CNT === 0) {
-        res.send({success: false, msg: '존재하지 않는 아이디입니다.'});
-        connection.release();
-        return;
-      }
-
-      connection.query(mybatisMapper.getStatement('youngjak', 'getUser', param, format), function (error, results, fields) {
-        if (error) {
-          res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-          connection.release();
-          return;
-        }
-
-        if(results.length !== 1) {
-          res.send({success: false, msg: '비밀번호가 일치하지 않습니다.'});
-          connection.release();
-          return;
-        }
-
-        // const macAddr = JSON.stringify(macaddress.networkInterfaces(), null, 5)).wi;
-        macaddress.one(function (err, mac) {
-          if(err) {
-            res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-            connection.release();
-            return;
-          }
-
-          connection.release();
-
-          let userAuth = results[0].USER_AUTH;
-          console.log(mac);
-
-          if(userAuth === 'A') {
-            res.send({success: true, msg: '', 
-              token: jwt.sign({ userId: param.userId, mac: mac }, process.env.JWT_SECRET_KEY), 
-              token1: jwt.sign({ userAuth: userAuth, mac: mac }, process.env.JWT_SECRET_KEY), });
-          } else {
-            res.send({success: true, msg: '', token: jwt.sign({ userId: param.userId, mac: mac }, process.env.JWT_SECRET_KEY)});
-          }
-        });
-      });
-    });
-  });
-});
-
-router.post('/setText', async function(req, res, next) {
-  const param = req.body;
-  let success = true;
-  let msg = '';
-  let mac;
-  let decoded;
-  let decoded1;
-  console.log('1111111');
-  console.log(param.token);
-  console.log(param.token1);
-
-  if(param.token1 === null || param.token1 === undefined) {
-    console.log('쓰기 권한이 없습니다');
-    res.send({success: false, msg: '쓰기 권한이 없는 계정입니다.'});
-    return;
-  }
-
-  if(param.token === null || param.token === undefined) {
-    res.send({success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'});
-    return;
-  }
+  const connection = await pool.getConnection(async conn => conn);
 
   try {
-    mac = await macaddress.one();
-  } catch (err) {
-    console.log('mac err');
-    res.send({success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'});
-    return;
-  }
+    let [results] = await connection.query(mybatisMapper.getStatement('youngjak', 'getUserIdCnt', param, format));
 
-  try {
-    decoded = jwt.verify(param.token, process.env.JWT_SECRET_KEY);
-    decoded1 = jwt.verify(param.token1, process.env.JWT_SECRET_KEY);
-  } catch (err) {
-    console.log('jwt err');
-    res.send({success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'});
-    return;
-  }
-
-  if(mac !== decoded.mac || mac !== decoded1.mac) {
-    res.send({success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'});
-    return;
-  }
-
-  if(param.textKr === undefined || param.textKr === null || param.textKr === '') {
-    success = false;
-    msg = '한글 문장은 필수값입니다.';
-  }else if(param.textEn === undefined || param.textEn === null || param.textEn === '') {
-    success = false;
-    msg = '영문 문장은 필수값입니다.';
-  }
-
-  if(!success) {
-    res.send({success: success, msg: msg});
-    return;
-  }
-
-  param.userId = decoded.userId;
-
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      console.log('getConnection err');
-      res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-      return;
-    }
-
-    connection.beginTransaction(function(err) {
-      if (err) {
-        console.log('begintran err');
-        res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-        connection.release();
-        return;
-      }
-
-      connection.query(mybatisMapper.getStatement('youngjak', 'inserText', param, format), function (error, results, fields) {
-        if (error) {
-          console.log('inserText error : ' + error);
-          connection.rollback();
-          res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-          connection.release();
-          return;
-        }
-
-        connection.commit();
-
-        connection.release();
-        
-        res.send({success: true, msg: '데이터 저장 성공.'});
-      });
-    });
-  });
-});
-
-router.get('/getTextOneRandom', async function(req, res, next) {
-  const param = req.body;
-
-  pool.getConnection(function (err, connection) {
-    if (err) {
-      console.log('getConnection err');
-      res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-      return;
-    }
-
-    connection.query(mybatisMapper.getStatement('youngjak', 'selectTextOneRandom', param, format), function (error, results, fields) {
-      if (error) {
-        console.log('selectTextOneRandom error : ' + error);
-        connection.release();
-        return;
-      }
-
+    if(results[0].USER_ID_CNT === 0) {
       connection.release();
-      
-      res.send({success: true, msg: '', results});
-    });
-  });
+      res.send({success: false, msg: '존재하지 않는 아이디입니다.'});
+      return;
+    }
+
+    [results] = await connection.query(mybatisMapper.getStatement('youngjak', 'getUser', param, format));
+
+    if(results.length !== 1) {
+      connection.release();
+      res.send({success: false, msg: '비밀번호가 일치하지 않습니다.'});
+      return;
+    }
+
+    const mac = await macaddress.one();
+
+    let userAuth = results[0].USER_AUTH;
+
+    if(userAuth === 'A') {
+      res.send({success: true, msg: '', 
+        token: jwt.sign({ userId: param.userId, mac: mac }, process.env.JWT_SECRET_KEY), 
+        token1: jwt.sign({ userAuth: userAuth, mac: mac }, process.env.JWT_SECRET_KEY), });
+    } else {
+      res.send({success: true, msg: '', token: jwt.sign({ userId: param.userId, mac: mac }, process.env.JWT_SECRET_KEY)});
+    }
+  } catch (err) {
+    res.send({success: false, msg: '데이터 조회 실패.'});
+    return;
+  } finally {
+      connection.release();
+  }
 });
 
 
-// router.post('/setText', function(req, res, next) {
-//   const param = req.body;
 
-//   if(param.userId === undefined || param.userId === null || param.userId === '') {
-//     res.send({success: false, msg: '아이디는 필수값입니다.'});
-//     return;
-//   }
+const chkValid = async (pathName, param) => {
+  let mac;
 
-//   if(param.textKr === undefined || param.textKr === null || param.textKr === '') {
-//     res.send({success: false, msg: '한글 문장은 필수값입니다.'});
-//     return;
-//   }
+  if(pathName === '/setText') {
+    if(param.token1 === null || param.token1 === undefined) {
+      return {success: false, msg: '쓰기 권한이 없는 계정입니다.'};
+    }
 
-//   if(param.textEn === undefined || param.textEn === null || param.textEn === '') {
-//     res.send({success: false, msg: '영문 문장은 필수값입니다.'});
-//     return;
-//   }
-
-//   connection.query(mybatisMapper.getStatement('youngjak', 'getUserIdCnt', param, format), function (error, results, fields) {
-//     if (error) {
-//       res.send({success: false, msg: '데이터 처리 실패. 다시 시도하세요.'});
-//       return;
-//     }
-
-//     if(results[0].USER_ID_CNT === 0) {
-//       res.send({success: false, msg: '존재하지 않는 아이디입니다.'});
-//       return;
-//     }
-
-//     connection.query(mybatisMapper.getStatement('youngjak', 'getUserPwdCnt', param, format), function (error, results, fields) {
-//       if (error) {
-//         res.send({success: 1, msg: '데이터 처리 실패. 다시 시도하세요.'});
-//         return;
-//       }
-
-//       if(results[0].USER_ID_CNT) {
-//         res.send({success: false, msg: '비밀번호가 일치하지 않습니다.'});
-//         return;
-//       }
+    if(param.token === null || param.token === undefined) {
+      return {success: false, errName: 'JWT_FAIL', msg: '쓰기 권한이 없는 계정입니다.'};
+    }
   
-//       res.send({success: true, msg: '' });
-//     });
-//   });
-// });
+    try {
+      mac = await macaddress.one();
+    } catch (err) {
+      return {success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'};
+    }
+  
+    try {
+      decoded = jwt.verify(param.token, process.env.JWT_SECRET_KEY);
+      decoded1 = jwt.verify(param.token1, process.env.JWT_SECRET_KEY);
+    } catch (err) {
+      return {success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'};
+    }
 
-// router.post('/loginCheck', function(req, res, next) {
-//   // console.log('1');
-//   // console.log(req.body.token);
-//   // console.log('2');
-//   jwt.verify(req.body.token, process.env.JWT_SECRET_KEY, function(err, decoded) {
-//     if(err) {
-//       // console.log(err);
-//       res.send({success: false, msg: ''});
-//       return;
-//     }
+    if(mac !== decoded.mac || mac !== decoded1.mac) {
+      return {success: false, errName: 'JWT_FAIL', msg: '인증되지 않았습니다. 로그인 후 이용해주세요.'};
+    }
+  
+    if(param.textKr === undefined || param.textKr === null || param.textKr === '') {
+      return {success: false, msg: '한글 문장은 필수값입니다.'};
+    }
+    
+    if(param.textEn === undefined || param.textEn === null || param.textEn === '') {
+      return {success: false, msg: '영문 문장은 필수값입니다.'};
+    }
+  } else if(pathName === 'login') {
 
-//     // console.log(decoded.userId);
-//     res.send({success: true, msg: ''});
-//   });
-// });
+  }
 
-// router.get('/tts', async(req, res, next) => {
-//   console.log(req.query);
-//   const request = {
-//     input: {text: req.query.text},
-//     voice: {languageCode: req.query.lang, ssmlGender: req.query.voice},
-//     audioConfig: {audioEncoding: 'LINEAR16', speakingRate : 1},
-//   };
-
-//   const [response] = await client.synthesizeSpeech(request);
-
-//   res.send(response.audioContent);
-// });
+  return {success: true, msg: ''};
+}
 
 module.exports = router;
